475f8316f346475778cc6641217c05dc8a52c529
GanaF4ll <ganafall9498@gmail.com>
2024-12-18 16:07:49 +0100

    🔧 tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,10 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        green: "var(--green)",
+        blue: "var(--blue)",
+        orange: "var(--orange)",
+        white: "var(--white)",
       },
     },
   },
