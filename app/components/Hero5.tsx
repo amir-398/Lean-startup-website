70addaabfd2da92ae571f9e02bcb5f1cec8d88b4
GanaF4ll <ganafall9498@gmail.com>
2024-12-19 11:04:03 +0100

    🚧 HERO5

---
+++ app/components/Hero5.tsx
@@ -0,0 +1,130 @@
+import React from "react";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "./Accordion";
+import Image from "next/image";
+
+const Hero5 = () => {
+  return (
+    <section className="flex flex-col px-20 py-10 rounded-3xl font-medium items-center ">
+      <div className="flex p-5">
+        <div className="flex flex-col w-1/2 p-5 flex-shrink-0 overflow-auto">
+          <p className="text-orange font-semibold flex items-center">FAQ</p>
+          <h3 className="font-bold text-3xl">
+            Une question ? Voici quelques réponses.
+          </h3>
+          <p className="text-grey text-sm leading-loose">
+            Nous avons rassemblé les interrogations les plus fréquentes pour
+            t’aider à mieux comprendre notre application. Si tu ne trouves pas
+            ta réponse ici, notre équipe est toujours prête à t’aider !
+          </p>
+        </div>
+        <div className="flex flex-col w-1/2 p-5">
+          <Accordion type="single" collapsible>
+            <AccordionItem value="item-1">
+              <AccordionTrigger>
+                Comment fonctionne l’application ?
+              </AccordionTrigger>
+              <AccordionContent>
+                Yes. It adheres to the WAI-ARIA design pattern.
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
+
+          <Accordion type="single" collapsible>
+            <AccordionItem value="item-1">
+              <AccordionTrigger>
+                Quels sports sont disponibles ?
+              </AccordionTrigger>
+              <AccordionContent>
+                Yes. It adheres to the WAI-ARIA design pattern.
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
+
+          <Accordion type="single" collapsible>
+            <AccordionItem value="item-1">
+              <AccordionTrigger>
+                Puis-je organiser un événement privé ?
+              </AccordionTrigger>
+              <AccordionContent>
+                Yes. It adheres to the WAI-ARIA design pattern.
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
+
+          <Accordion type="single" collapsible>
+            <AccordionItem value="item-1">
+              <AccordionTrigger>
+                Comment fonctionne le système de niveaux ?
+              </AccordionTrigger>
+              <AccordionContent>
+                Yes. It adheres to the WAI-ARIA design pattern.
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
+
+          <Accordion type="single" collapsible>
+            <AccordionItem value="item-1">
+              <AccordionTrigger>
+                Comment fonctionne le système de matchmaking ?
+              </AccordionTrigger>
+              <AccordionContent>
+                Yes. It adheres to the WAI-ARIA design pattern.
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
+
+          <Accordion type="single" collapsible>
+            <AccordionItem value="item-1">
+              <AccordionTrigger>
+                Puis-je réserver des terrains directement depuis l’application ?
+              </AccordionTrigger>
+              <AccordionContent>
+                Yes. It adheres to the WAI-ARIA design pattern.
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
+
+          <Accordion type="single" collapsible>
+            <AccordionItem value="item-1">
+              <AccordionTrigger>
+                Que faire si personne ne rejoint mon match ?
+              </AccordionTrigger>
+              <AccordionContent>
+                Yes. It adheres to the WAI-ARIA design pattern.
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
+        </div>
+      </div>
+      <div className="bg-orange w-50 h-50 rounded-xl w-1/2 px-20 py-10 relative test4 overflow-hidden">
+        <h3 className="font-bold text-3xl text-white">
+          Prêt à rejoindre le jeu ?{" "}
+        </h3>
+        <br />
+        <p className="text-white font-normal z-index-10">
+          Match ou pas match ? la balle est dans ton camp !
+        </p>
+        <br />
+        <button className="bg-white rounded-3xl font-semibold p-3">
+          <p>
+            <span className="text-orange">GO!</span> m'inscrire
+          </p>
+        </button>
+        <Image
+          src="/basketball-black.svg"
+          alt="basketball"
+          width={300}
+          height={300}
+          className="absolute right-0 top-0 z-index-0"
+        />
+      </div>
+    </section>
+  );
+};
+
+export default Hero5;
--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Hero1 from "./components/Hero1";
 import Hero2 from "./components/Hero2";
 import Hero3 from "./components/Hero3";
 import Hero4 from "./components/Hero4";
+import Hero5 from "./components/Hero5";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <Hero2 />
       <Hero3 />
       <Hero4 />
+      <Hero5 />
     </div>
   );
 }
