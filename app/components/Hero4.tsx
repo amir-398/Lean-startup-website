8ec3aee7ba3fd3c7c2e11cc62e153003d8d3a8e3
GanaF4ll <ganafall9498@gmail.com>
2024-12-19 09:45:55 +0100

    ✨ Hero4

---
+++ app/components/Hero4.tsx
@@ -0,0 +1,92 @@
+import Image from "next/image";
+import React from "react";
+
+const Hero4 = () => {
+  return (
+    <section className="flex flex-col px-20 py-10 rounded-3xl font-medium items-center bg-white2">
+      <p className="text-orange font-semibold flex items-center">MATCHMAKING</p>
+
+      <h3 className="font-bold text-3xl">
+        Laisse notre algorithme faire le reste.
+      </h3>
+      <div className="flex">
+        <div className="flex flex-wrap w-1/2 p-5">
+          <div className="flex flex-col  w-1/2">
+            <div className="p-3 rounded-lg bg-white3 w-1/6 flex justify-center">
+              <Image
+                src="/location.svg"
+                alt="location icon"
+                width={24}
+                height={24}
+              />
+            </div>
+            <div>
+              <p className="text-xl font-medium">Localisation optimisée</p>
+              <p className="text-grey text-sm leading-loose">
+                Il identifie les matchs et équipes proches de toi pour minimiser
+                les déplacements.
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-col  w-1/2">
+            <div className="p-3 rounded-lg bg-white3 w-1/6 flex justify-center">
+              <Image
+                src="/sparkles.svg"
+                alt="sparkles picture"
+                width={24}
+                height={24}
+              />
+            </div>
+            <div>
+              <p className="text-xl font-medium">Matching par niveau</p>
+              <p className="text-grey text-sm leading-loose">
+                Il associe les joueurs et équipes selon leur niveau, pour des
+                parties équilibrées et motivantes.
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-col  w-1/2">
+            <div className="p-3 rounded-lg bg-white3 w-1/6 flex justify-center">
+              <Image
+                src="/preferences.svg"
+                alt="preferences icon"
+                width={24}
+                height={24}
+              />
+            </div>
+            <div>
+              <p className="text-xl font-medium">Préférences sportives</p>
+              <p className="text-grey text-sm leading-loose">
+                Il prend en compte tes sports favoris et tes disponibilités pour
+                des recommandations personnalisées.
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-col  w-1/2">
+            <div className="p-3 rounded-lg bg-white3 w-1/6 flex justify-center">
+              <Image src="/graph.svg" alt="graph icon" width={24} height={24} />
+            </div>
+            <div>
+              <p className="text-xl font-medium">MAJ en temps réel</p>
+              <p className="text-grey text-sm leading-loose">
+                Les matchs et joueurs disponibles sont actualisés en continu
+                pour t’offrir le meilleur choix.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="w-1/2 p-5">
+          <Image
+            src="/graph.png"
+            alt="graph"
+            width={500}
+            height={500}
+            className="bg-orange rounded-3xl mr-20"
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero4;
--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import Hero1 from "./components/Hero1";
 import Hero2 from "./components/Hero2";
 import Hero3 from "./components/Hero3";
+import Hero4 from "./components/Hero4";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <Hero1 />
       <Hero2 />
       <Hero3 />
+      <Hero4 />
     </div>
   );
 }
